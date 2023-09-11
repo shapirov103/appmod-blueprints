24a6d161565349a28fc1a372e76b73ca497d229f
shapirov <shapirov@amazon.com>
2023-09-11 00:05:08 -0400

    WIP

--- platform/infra/lib/blueprints/hybrid.ts
+++ platform/infra/lib/blueprints/hybrid.ts
@@ -2,6 +2,8 @@ import { Construct } from 'constructs';
 import * as blueprints from '@aws-quickstart/eks-blueprints';
 import { KubevelaAddon } from '../addons/kubevela';
 
+const GIT_URL = "https://github.com/shapirov103/appmod-blueprints";
+
 export default class HybridCluster {
     static build(scope: Construct) {
         blueprints.EksBlueprint.builder()
@@ -11,7 +13,16 @@ export default class HybridCluster {
                 new blueprints.VpcCniAddOn, 
                 new blueprints.MetricsServerAddOn,
                 new blueprints.ClusterAutoScalerAddOn,
-                new KubevelaAddon()
+                new KubevelaAddon(),
+                new blueprints.SecretsStoreAddOn(),
+                new blueprints.ArgoCDAddOn({
+                    bootstrapRepo: {
+                        repoUrl: GIT_URL,
+                        path: 'platform/gitops/addons/mongo',
+                        credentialsSecretName: 'github-ssh-key',
+                        credentialsType: 'SSH'
+                    }
+                })
             )
             .build(scope, "hybrid-cluster");
     }
