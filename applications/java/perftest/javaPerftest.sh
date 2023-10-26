de45f14d8c5166c3b06ae7e50a87d9e5a396d4a6
Ubuntu <ubuntu@ip-172-31-53-134.us-west-2.compute.internal>
2023-10-26 00:55:33 +0000

    Added parameter in perf test

--- applications/java/perftest/javaPerftest.sh
+++ applications/java/perftest/javaPerftest.sh
@@ -2,28 +2,34 @@
 echo "This script is performing the Performance test for Java application"
 
 echo "Based on the input parameter, color marked will be deemded as functionality failure"
-echo "Please enter the Java application ingress url"
+echo "Please enter the Java application ingress url and the expected average request time"
 echo "The number of arguments is: $#"
 a=$#
-if [ $a -le 1 ]
+if [ $a -lt 2 ]
 then
  
-echo "One parameter is expected"
+echo "Atleast two parameters are expected"
 exit 1
 
 fi
 
 INGRESS_URL=$1
+REQUESTED_TIMEOUT=`echo $2`
+
+INT_REQUEST_TO=${REQUESTED_TIMEOUT%.*}
+
 touch /tmp/perfTestResults.out
 >/tmp/perfTestResults.out
-ab -n 10 -c 10 -k "http://$INGRESS_URL/java-app"> /tmp/perfTestResults.out
+ab -n 1000 -c 10 -k "http://$INGRESS_URL/java-app"> /tmp/perfTestResults.out
 cat /tmp/perfTestResults.out
 
 TotalTimeRequest=`cat /tmp/perfTestResults.out| grep "Time per request" |head -1 | awk -F':' '{print $2}'| awk -F' ' '{print $1}'`
 echo "Total Time per Request is: $TotalTimeRequest ms"
 DECIMAL=`echo $TotalTimeRequest`
 INTEGER=${DECIMAL%.*}
-if [ "$INTEGER" -le 5 ]; then
+echo "Integer is $INTEGER"
+echo "RTO uis $INT_REQUEST_TO"
+if [ "$INTEGER" -le "$INT_REQUEST_TO" ]; then
 	echo "Performance test PASSED, average request time is: $TotalTimeRequest ms"
 	exit 0
 else
